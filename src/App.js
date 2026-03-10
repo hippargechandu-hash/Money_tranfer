9efa1f82933784606c5f821623932f48873a4796
Hipparge Chandrashekar <hippargechandu.com>
2026-03-10 16:19:10 +0530

    first repo

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
+import React from 'react'
+import Money from  "./Componet/MoneyTransform/Money.js"
+function App() {
+  return (
+    <div>
+      <Money/>
+    </div>
+  )
+}
 
+export default App
